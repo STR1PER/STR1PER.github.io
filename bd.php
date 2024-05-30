3d779a8b9130efa7a362243f99f024bec2bd6410
STR1PER <153841612+STR1PER@users.noreply.github.com>
2024-05-30 11:30:50 +0300

    Add files via upload

--- bd.php
+++ bd.php
@@ -1,1 +1,19 @@
+<?php
+$servername = "localhost"; // адрес сервера базы данных
+$username = "root"; // имя пользователя базы данных
+$password = ""; // пароль пользователя базы данных
+$dbname = "Клиент"; // название вашей базы данных
+
+// Создание соединения
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверка соединения
+if ($conn->connect_error) {
+    die("Ошибка соединения: " . $conn->connect_error);
+}
+?>
+<?php
+// Закрытие соединения
+$conn->close();
+?>
 
